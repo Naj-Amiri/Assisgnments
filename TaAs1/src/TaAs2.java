7277ee692dbb1f9933c32faaa4571a8ff5ef43f6
Naj <naj.amiri@yahoo.com>
2022-07-01 11:21:07 -0700

    Test2

--- TaAs1/src/TaAs2.java
+++ TaAs1/src/TaAs2.java
@@ -1,8 +1,14 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TaAs2 {
 
 	public static void main(String[] args) {
-	
+		System.setProperty("webdriver.chrome.driver", "/Users/naj/Drivers/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		System.out.println("Done");
+		
 
 	}
 
