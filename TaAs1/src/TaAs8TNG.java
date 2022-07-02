9d1be078a9b1353b89cfd9cc6d3fca9b63d0b7dd
Administrator <Administrator@10.0.0.41>
2022-07-02 00:43:39 -0400

    FrameWork (TestNG) Class

---
+++ TaAs1/src/TaAs8TNG.java
@@ -0,0 +1,15 @@
+import org.testng.annotations.Test;
+
+public class TaAs8TNG {
+
+	
+	
+	@Test
+	public void TestNGClass()
+	{
+		
+		System.out.println(" I hope you all doing great");
+		
+		
+	}
+}
