754bb70386bb2d91a9586b3e58131dbb175f7f3d
esmat <esmat@10.0.0.192>
2022-07-05 22:53:37 -0700

    new commit added

--- TaAs1/src/TaAs1.java
+++ TaAs1/src/TaAs1.java
@@ -5,14 +5,15 @@ public class TaAs1 {
 
 	public static void main(String[] args) {
 		
-		System.setProperty("Webdriver.chrome.driver\", \"D:\\Recup Selenium\\drivers\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver_win32(1)\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.facebook.com/");
 //<<<<<<< HEAD
 //=======
 		driver.manage().window().fullscreen();
 		driver.close();
-		System.out.println("kabul")
+		System.out.println("kabul");
+		System.out.println("Afghanistan");
 		
 		
 //>>>>>>> branch 'master' of https://github.com/Naj-Amiri/Assisgnments.git
